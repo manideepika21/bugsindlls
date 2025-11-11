5e738e4a959a9087a5bc41d3f3c83d9ac31e0b7d
dvalent2 <dvalent2@ncsu.edu>
2025-11-11 13:11:40 -0500

    test

---
+++ tensorflow/issue_97096/buggy_code.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+
+print(tf.__version__)   # 2.20.0-dev20250715
+rng = np.random.default_rng(311)
+
+input_tensor = tf.constant(rng.uniform(-np.finfo(np.float16).max-1, 0., size=(1, 2)), dtype=tf.float16)
+axis = tf.constant(0, dtype=tf.int16)
+output_type = tf.int16
+
+output = tf.math.argmax(input_tensor, axis=axis, output_type=output_type)
